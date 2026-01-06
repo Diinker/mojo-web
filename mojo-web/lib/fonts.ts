97206f213111f0faccd84469b44a47786117a68c
Zack Yuan <zack6yuan@gmail.com>
2026-01-05 18:30:26 -0600

    updated file structure.

---
+++ mojo-web/lib/fonts.ts
@@ -0,0 +1,21 @@
+import localFont from "next/font/local";
+
+export const beyonders = localFont({
+  src: "../public/fonts/Beyonders.ttf",
+  variable: "--font-beyonders",
+});
+
+export const coolvetica = localFont({
+  src: "../public/fonts/Coolvetica.otf",
+  variable: "--font-coolvetica",
+});
+
+export const nature = localFont({
+  src: "../public/fonts/Nature.ttf",
+  variable: "--font-nature",
+});
+
+export const natureLight = localFont({
+  src: "../public/fonts/Nature-Light.ttf",
+  variable: "--font-nature-light",
+});
